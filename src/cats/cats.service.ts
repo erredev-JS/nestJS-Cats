5381f41a15dcd7a51b12ba5035adc4a17eb59748
erredev-JS <rodrigozapata968@gmail.com>
2025-07-02 16:19:16 -0300

    REST API Without validations

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -32,10 +32,10 @@ export class CatsService {
   }
 
   async update(id: number, updateCatDto: UpdateCatDto) {
-    return `This action updates a #${id} cat`;
+    return await this.catRepository.update({id}, updateCatDto)
   }
 
   async remove(id: number) {
-    return `This action removes a #${id} cat`;
+    return await this.catRepository.softDelete({id});
   }
 }
